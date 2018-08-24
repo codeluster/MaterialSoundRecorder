45616f630516c558c0f69baffa14c8bc5279bc66
tanmay <tanmaysingal@icloud.com>
2018-08-24 15:29:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tanmay/soundrecorder/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tanmay.soundrecorder;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class FragmentAdapter extends FragmentPagerAdapter{
+
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+
+}

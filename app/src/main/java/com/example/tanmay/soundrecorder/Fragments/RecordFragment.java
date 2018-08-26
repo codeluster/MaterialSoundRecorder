cfe9387d16cbcdd521de9feb0330ba85fa8fa412
tanmay <tanmaysingal@icloud.com>
2018-08-27 00:32:30 +0530

    Built functionality to start recording!

--- app/src/main/java/com/example/tanmay/soundrecorder/Fragments/RecordFragment.java
+++ app/src/main/java/com/example/tanmay/soundrecorder/Fragments/RecordFragment.java
@@ -1,15 +1,21 @@
 package com.example.tanmay.soundrecorder.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 import com.example.tanmay.soundrecorder.R;
+import com.example.tanmay.soundrecorder.Services.RecordingService;
+
+import java.io.File;
 
 public class RecordFragment extends Fragment {
 
@@ -42,7 +48,19 @@ public class RecordFragment extends Fragment {
 
     private void onStartRecording() {
 
+        // Intent to launch service
+        Intent service = new Intent(getActivity(), RecordingService.class);
 
+        File folder = new File(Environment.getExternalStorageDirectory() + "/" + getString(R.string.default_file_directory));
+        // Create folder if it doesn't exist
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        // Start recording
+        getActivity().startService(service);
+        // Inform the user
+        Toast.makeText(getActivity(), getString(R.string.prompt_recording_started), Toast.LENGTH_SHORT).show();
 
     }
 

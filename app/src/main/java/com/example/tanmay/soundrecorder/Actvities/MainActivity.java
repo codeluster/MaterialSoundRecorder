17d6bc0700fa3c6114f3f3e9bbf256c5a03f0b86
tanmay <tanmaysingal@icloud.com>
2018-08-29 23:45:01 +0530

    Built SettingsFragment

--- app/src/main/java/com/example/tanmay/soundrecorder/Actvities/MainActivity.java
+++ app/src/main/java/com/example/tanmay/soundrecorder/Actvities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tanmay.soundrecorder.Actvities;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,6 +8,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tanmay.soundrecorder.Fragments.PreviousRecordingsFragment;
 import com.example.tanmay.soundrecorder.Fragments.RecordFragment;
@@ -30,7 +33,24 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-    private class SectionsPagerAdapter extends FragmentPagerAdapter{
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.preferences_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_open_settings:
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         // An array containing the list of titles
         private String[] titles = getResources().getStringArray(R.array.fragment_titles);
@@ -44,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
 
-            switch (position){
+            switch (position) {
                 case 0:
                     return new RecordFragment();
                 case 1:
--- app/src/main/java/com/example/tanmay/soundrecorder/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/tanmay/soundrecorder/Fragments/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package com.example.tanmay.soundrecorder.Fragments
 
 import android.os.Bundle
+import android.preference.CheckBoxPreference
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import com.example.tanmay.soundrecorder.Data.UserPreferences
 import com.example.tanmay.soundrecorder.R
 
 class SettingsFragment : PreferenceFragment() {
@@ -10,6 +13,16 @@ class SettingsFragment : PreferenceFragment() {
         super.onCreate(savedInstanceState)
 
         addPreferencesFromResource(R.xml.preferences_fragment)
+
+        val highQualityPref = findPreference(getString(R.string.pref_high_quality_key)) as CheckBoxPreference
+
+        // Set status of checkbox on basis of previous configuration
+        highQualityPref.isChecked = UserPreferences.getQualityPref(activity)
+
+        highQualityPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+            UserPreferences.setQualityPref(activity, newValue as Boolean)
+            true
+        }
 
     }
 }

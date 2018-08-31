cbadac0127a510fd1b9c8f1ae64b9851420f4437
tanmay <tanmaysingal@icloud.com>
2018-08-31 21:57:00 +0530

    Tested CRUDops on database

--- app/src/main/java/com/example/tanmay/soundrecorder/Actvities/TestActivity.java
+++ app/src/main/java/com/example/tanmay/soundrecorder/Actvities/TestActivity.java
@@ -1,9 +1,12 @@
 package com.example.tanmay.soundrecorder.Actvities;
 
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.example.tanmay.soundrecorder.Data.RecordingItem;
@@ -12,10 +15,17 @@ import com.example.tanmay.soundrecorder.R;
 
 public class TestActivity extends AppCompatActivity {
 
+    private ContentResolver resolver;
+    private String LOG_TAG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        LOG_TAG = TestActivity.class.getSimpleName();
+
+        resolver = getContentResolver();
 
         RecordingItem recordingItem = new RecordingItem("rec1", "path1", 18, 230, 3489);
 
@@ -26,7 +36,13 @@ public class TestActivity extends AppCompatActivity {
         values.put(RecordingsContract.RecordingsEntry.COLUMN_RECORDING_LENGTH, recordingItem.getLength());
         values.put(RecordingsContract.RecordingsEntry.COLUMN_RECORDING_TIME, recordingItem.getTime());
 
-        getContentResolver().insert(RecordingsContract.RecordingsEntry.CONTENT_URI, values);
+        resolver.insert(RecordingsContract.RecordingsEntry.CONTENT_URI, values);
+
+        Cursor cursor = resolver.query(RecordingsContract.RecordingsEntry.CONTENT_URI, null, null, null, null, null);
+
+        Log.v(LOG_TAG, "Received cursor. Cursor count = " + cursor.getCount());
+
+        resolver.delete(RecordingsContract.RecordingsEntry.CONTENT_URI, null, null);
 
     }
 }

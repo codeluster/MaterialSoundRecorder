db8b4e7b3be6e5c83a618d9ea2406e33a4610279
Tanmay Singal <tanmaysingal@icloud.com>
2018-09-01 11:51:09 +0530

    Handoff commit

--- app/src/main/java/com/example/tanmay/soundrecorder/Fragments/PreviousRecordingsFragment.java
+++ app/src/main/java/com/example/tanmay/soundrecorder/Fragments/PreviousRecordingsFragment.java
@@ -24,7 +24,10 @@ public class PreviousRecordingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_previous_recordings, container, false);
+        View v = inflater.inflate(R.layout.fragment_previous_recordings, container, false);
+
+
+        return v;
     }
 
 }
